5e82fd3913f7a20be90e7481a7e0968ac632c6c3
Rodgers31 <otienor986@gmail.com>
2025-03-09 14:28:26 -0500

    Buttons and Inputs

--- Day27_Tkinter/main.py
+++ Day27_Tkinter/main.py
@@ -1,13 +1,35 @@
-import tkinter
+from tkinter import *
+# import tkinter
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
+# my_label.pack(side="left")
 
+my_label["text"] = "New text"
+my_label.config(text="New text2")
+
+# Button
+
+
+def button_clicked():
+    # my_label["text"] = "Button got clicked"
+    new_text = input.get()
+    my_label.config(text=new_text)
+    print("I got clicked")
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+
+input = Entry(width=10)
+input.pack()
 
 
 window.mainloop()
--- Day27_Tkinter/playground.py
+++ Day27_Tkinter/playground.py
@@ -9,3 +9,30 @@ def add(*args):
 
 result = add(5, 7, 8, 20, 45, 99, 193, 45)
 print(result)
+
+# Adding key word to your function
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw["make"]
+        self.model = kw["model"]
+        # get will make the code not fail even if the value is not passed
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.model)
