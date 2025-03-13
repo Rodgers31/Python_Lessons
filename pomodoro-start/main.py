a93032171e49b9a0e773ae61eccb61ea8919a243
Rodgers31 <otienor986@gmail.com>
2025-03-12 23:36:06 -0500

    reset button

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -8,11 +8,25 @@ GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 1
-SHORT_BREAK_MIN = 5
+SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+
+def reset_timer():
+    global reps
+    window.after_cancel(timer)
+    # timer_text = 00:00
+    canvas.itemconfig(timer_text, text="00:00")
+    # title_label = "Timer"
+    title_label.config(text="Timer")
+    # reset check_marks
+    reps = 0
+
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
@@ -23,20 +37,21 @@ def start_timer():
     reps += 1
     # if it's the 1st/3rd/5th/7th rep:
     if reps % 2 > 0:
-        timer.config(text="Work", fg=GREEN)
+        title_label.config(text="Work", fg=GREEN)
         count_down(work_sec)
     # if it's the 8th rep:
     elif reps == 8:
-        timer.config(text="Break", fg=RED)
+        title_label.config(text="Break", fg=RED)
         count_down(long_break_sec)
     # if it's the 2nd/4th/6th rep:
     else:
-        timer.config(text="Break", fg=PINK)
+        title_label.config(text="Break", fg=PINK)
         count_down(short_break_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
+    global reps
     count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_sec < 10:
@@ -46,10 +61,16 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+      global timer
+      timer = window.after(1000, count_down, count - 1)
 
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✅"
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -67,16 +88,16 @@ canvas.grid(column=1, row=1)
 
 
 # Entry
-timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "normal"))
-timer.grid(column=1, row=0)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "normal"))
+title_label.grid(column=1, row=0)
 # Start Button
 start_button = Button(text="Start",  highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 # End Button
-end_button = Button(text="Reset",  highlightthickness=0)
+end_button = Button(text="Reset",  highlightthickness=0, command=reset_timer)
 end_button.grid(column=2, row=2)
 # Check
-check_marks = Label(text="✅", fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 window.mainloop()
