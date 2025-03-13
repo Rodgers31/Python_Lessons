e0cabd56da65470cc6abd9851e6180eaaa0e1ac7
Rodgers31 <otienor986@gmail.com>
2025-03-12 20:22:52 -0500

    Countdown function

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -13,8 +14,19 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    # To get 5 minutes, multiply by 60
+    count_down(25 * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -22,20 +34,11 @@ window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
 
-def start_timer():
-    time = 25
-    while time > 0:
-        time -= 1
-        canvas.itemconfig(timer_text, text=f"{time}")
-    window.after(1000, start_timer)
-
-
-c
-anvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 # x and y value for which the image will be displayed the width and height
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="25:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 
@@ -43,11 +46,13 @@ canvas.grid(column=1, row=1)
 timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "normal"))
 timer.grid(column=1, row=0)
 # Start Button
-start_button = Button(text="Start", command=start_timer)
+start_button = Button(text="Start",  highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 # End Button
-end_button = Button(text="End")
+end_button = Button(text="Reset",  highlightthickness=0)
 end_button.grid(column=2, row=2)
 # Check
+check_marks = Label(text="✅", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
