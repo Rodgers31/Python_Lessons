6b143986100d0a8f320b90fc7399d40a9b3a10e7
Rodgers31 <otienor986@gmail.com>
2025-03-12 22:57:19 -0500

    Countdown breaks

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -7,25 +7,49 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    # To get 5 minutes, multiply by 60
-    count_down(25 * 60)
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    reps += 1
+    # if it's the 1st/3rd/5th/7th rep:
+    if reps % 2 > 0:
+        timer.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+    # if it's the 8th rep:
+    elif reps == 8:
+        timer.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    # if it's the 2nd/4th/6th rep:
+    else:
+        timer.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    elif count_sec == 0:
+        count_sec = "00"
+
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
