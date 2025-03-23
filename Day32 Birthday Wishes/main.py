16b288f1624a2d0a0407b9147410ee3160f605de
Rodgers31 <otienor986@gmail.com>
2025-03-23 14:41:39 -0500

    Day 32 smtp setup

--- Day32 Birthday Wishes/main.py
+++ Day32 Birthday Wishes/main.py
@@ -1,7 +1,11 @@
 import smtplib
 
 my_email = "rodgerdoger003@gmail.com"
-password = "xxxxxxx"
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
+password = "qyovfibexhjqoris"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="rodgersotieno@myyahoo.com",
+                        msg="Subject:Hello\n\nThis is the body of my email")
+
