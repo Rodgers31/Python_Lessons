be3756a3c775e57321a708bef5baccd2d8c686f8
Rodgers31 <otienor986@gmail.com>
2025-03-27 21:27:17 -0500

    completed quiz app

--- quizzler_app/data.py
+++ quizzler_app/data.py
@@ -2,7 +2,8 @@ import requests
 
 parameters = {
     "amount": 10,
-    "type": "boolean"
+    "type": "boolean",
+    "category": 18
 }
 requestor = requests.get(url="https://opentdb.com/api.php", params=parameters)
 requestor.raise_for_status()
