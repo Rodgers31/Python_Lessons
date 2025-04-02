ba10d185e2d264bd01a30c9e50415d07f867dd21
Rodgers31 <otienor986@gmail.com>
2025-04-01 22:05:15 -0500

    getting the id for weather

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -4,12 +4,16 @@ api_key = "f88b9e7b8dfb56ea4ac4eca0c08c06a3"
 parameters = {
     "lat": 43.038902,
     "lon": -87.906471,
-    "appid": api_key
+    "appid": api_key,
+    "cnt": 4
 }
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast?", params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data["cod"])
-print(data["list"])
+for weather in data["list"]:
+    for weather_id in weather["weather"]:
+        if int(weather_id["id"]) < 700:
+            print("Bring an umbrella")
+
 
