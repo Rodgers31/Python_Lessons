382a391303bdfbc510278523c416621fb417e84f
Rodgers31 <otienor986@gmail.com>
2025-04-02 20:09:05 -0500

    stored key in env instead

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,19 +1,25 @@
 import requests
-api_key = "f88b9e7b8dfb56ea4ac4eca0c08c06a3"
+from twilio.rest import Client
+import os
+from dotenv import load_dotenv
 
+load_dotenv("weather.env")
 parameters = {
     "lat": 43.038902,
     "lon": -87.906471,
-    "appid": api_key,
+    "appid": os.getenv("weather_api_key"),
     "cnt": 4
 }
-
+condition_codes = []
+will_rain = False
 response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast?", params=parameters)
 response.raise_for_status()
 data = response.json()
 for weather in data["list"]:
-    for weather_id in weather["weather"]:
-        if int(weather_id["id"]) < 700:
-            print("Bring an umbrella")
+    condition_code = weather["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
 
 
+if will_rain:
+    print("Bring an umbrella")
