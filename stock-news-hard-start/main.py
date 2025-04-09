3a66cb0c4150846fcdf2ec9e92b17fd8499d1a3c
Rodgers31 <otienor986@gmail.com>
2025-04-08 20:59:49 -0500

    completed stock app

--- stock-news-hard-start/main.py
+++ stock-news-hard-start/main.py
@@ -4,6 +4,7 @@ import requests
 import os
 from dotenv import load_dotenv
 import datetime as dt
+from twilio.rest import Client
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -17,62 +18,87 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 #HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
 #HINT 2: Work out the value of 5% of yerstday's closing stock price.
 
-def formatter(my_year, my_month, my_day):
-    return f"{my_year}-{my_month:02d}-{my_day:02d}"
-
-
-now = dt.datetime.now()
-year = now.year
-month = now.month
-day = now.day
-day_of_week = now.weekday()
-yesterday = ''
-day_yesterday = ''
-if day_of_week == 0:
-    yesterday = formatter(year, month, day - 3)
-    day_yesterday = formatter(year, month, day - 4)
-elif day_of_week == 1:
-    yesterday = formatter(year, month, day - 1)
-    day_yesterday = formatter(year, month, day - 4)
-elif day_of_week == 6:
-    yesterday = formatter(year, month, day - 2)
-    day_yesterday = formatter(year, month, day - 3)
-else:
-    yesterday = formatter(year, month, day - 1)
-    day_yesterday = formatter(year, month, day - 2)
-
-
-parameters = {
-"function": "TIME_SERIES_DAILY",
-"symbol": STOCK,
-"interval": "5min",
-"apikey": os.getenv("alpha_vantage")
-
-}
-
-response = requests.get(url="https://www.alphavantage.co/query", params=parameters)
-response.raise_for_status()
-stock_data = response.json()
-print(stock_data)
-tesla_yesterday = stock_data['Time Series (Daily)'][yesterday]
-tesla_two_yesterday = stock_data['Time Series (Daily)'][day_yesterday]
-
-y_close = tesla_yesterday['4. close']
-
-close_percent = (5 * float(y_close) / 100)
-
-yy_close = tesla_two_yesterday['4. close']
-
-difference = float(y_close) - float(yy_close)
-
-if difference > close_percent:
-    print("Get news")
-print(close_percent)
+# def formatter(my_year, my_month, my_day):
+#     return f"{my_year}-{my_month:02d}-{my_day:02d}"
+#
+#
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day = now.day
+# day_of_week = now.weekday()
+# yesterday = ''
+# day_yesterday = ''
+# if day_of_week == 0:
+#     yesterday = formatter(year, month, day - 3)
+#     day_yesterday = formatter(year, month, day - 4)
+# elif day_of_week == 1:
+#     yesterday = formatter(year, month, day - 1)
+#     day_yesterday = formatter(year, month, day - 4)
+# elif day_of_week == 6:
+#     yesterday = formatter(year, month, day - 2)
+#     day_yesterday = formatter(year, month, day - 3)
+# else:
+#     yesterday = formatter(year, month, day - 1)
+#     day_yesterday = formatter(year, month, day - 2)
+#
+#
+# parameters = {
+# "function": "TIME_SERIES_DAILY",
+# "symbol": STOCK,
+# "interval": "5min",
+# "apikey": os.getenv("alpha_vantage")
+#
+# }
+#
+# response = requests.get(url="https://www.alphavantage.co/query", params=parameters)
+# response.raise_for_status()
+# stock_data = response.json()
+# print(stock_data)
+# tesla_yesterday = stock_data['Time Series (Daily)'][yesterday]
+# tesla_two_yesterday = stock_data['Time Series (Daily)'][day_yesterday]
+#
+# y_close = tesla_yesterday['4. close']
+#
+# close_percent = (5 * float(y_close) / 100)
+#
+# yy_close = tesla_two_yesterday['4. close']
+#
+# difference = float(y_close) - float(yy_close)
+#
+# if difference > close_percent:
+#     news_message(y_close, yy_close)
+# print(close_percent)
 ## STEP 2: Use https://newsapi.org/docs/endpoints/everything
 # Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
 #HINT 1: Think about using the Python Slice Operator
+def news_message():
+    parameters = {
+        "q": STOCK,
+        "apiKey": os.getenv("news_api")
 
+    }
+    direct = ''
+    if y_close > yy_close:
+        direct = '🔺'
+    else:
+        direct = '🔻'
 
+    response = requests.get(url="https://newsapi.org/v2/everything", params=parameters)
+    response.raise_for_status()
+    tesla_news = response.json()
+    for article in tesla_news['articles'][:3]:
+        headline = article['title']
+        description = article['description']
+        account_sid = os.getenv("account_sid")
+        auth_token = os.getenv("auth_token")
+
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            body=f"TSLA: {direct}{difference}%\n Headline: {headline}.\n Brief: {description}",
+            from_="whatsapp:+14155238886",
+            to="whatsapp:+14147797306",
+        )
 
 ## STEP 3: Use twilio.com/docs/sms/quickstart/python
 # Send a separate message with each article's title and description to your phone number. 
