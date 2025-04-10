0fda6a96b6f0210755e370ae436921115f15507e
Rodgers31 <otienor986@gmail.com>
2025-04-09 20:15:29 -0500

    project complete

--- stock-news-hard-start/main.py
+++ stock-news-hard-start/main.py
@@ -13,80 +13,21 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price.
-
-# def formatter(my_year, my_month, my_day):
-#     return f"{my_year}-{my_month:02d}-{my_day:02d}"
-#
-#
-# now = dt.datetime.now()
-# year = now.year
-# month = now.month
-# day = now.day
-# day_of_week = now.weekday()
-# yesterday = ''
-# day_yesterday = ''
-# if day_of_week == 0:
-#     yesterday = formatter(year, month, day - 3)
-#     day_yesterday = formatter(year, month, day - 4)
-# elif day_of_week == 1:
-#     yesterday = formatter(year, month, day - 1)
-#     day_yesterday = formatter(year, month, day - 4)
-# elif day_of_week == 6:
-#     yesterday = formatter(year, month, day - 2)
-#     day_yesterday = formatter(year, month, day - 3)
-# else:
-#     yesterday = formatter(year, month, day - 1)
-#     day_yesterday = formatter(year, month, day - 2)
-#
-#
-# parameters = {
-# "function": "TIME_SERIES_DAILY",
-# "symbol": STOCK,
-# "interval": "5min",
-# "apikey": os.getenv("alpha_vantage")
-#
-# }
-#
-# response = requests.get(url="https://www.alphavantage.co/query", params=parameters)
-# response.raise_for_status()
-# stock_data = response.json()
-# print(stock_data)
-# tesla_yesterday = stock_data['Time Series (Daily)'][yesterday]
-# tesla_two_yesterday = stock_data['Time Series (Daily)'][day_yesterday]
-#
-# y_close = tesla_yesterday['4. close']
-#
-# close_percent = (5 * float(y_close) / 100)
-#
-# yy_close = tesla_two_yesterday['4. close']
-#
-# difference = float(y_close) - float(yy_close)
-#
-# if difference > close_percent:
-#     news_message(y_close, yy_close)
-# print(close_percent)
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
-#HINT 1: Think about using the Python Slice Operator
-def news_message(y_close, yy_close, difference):
-    parameters = {
+def news_message(one_yesterday_close, two_yesterday_close, price_def):
+    news_parameters = {
         "q": STOCK,
         "apiKey": os.getenv("news_api")
 
     }
 
-    if y_close > yy_close:
+    if one_yesterday_close > two_yesterday_close:
         direct = '🔺'
     else:
         direct = '🔻'
 
-    response = requests.get(url="https://newsapi.org/v2/everything", params=parameters)
-    response.raise_for_status()
-    tesla_news = response.json()
+    news_response = requests.get(url="https://newsapi.org/v2/everything", params=news_parameters)
+    news_response.raise_for_status()
+    tesla_news = news_response.json()
     for article in tesla_news['articles'][:3]:
         headline = article['title']
         description = article['description']
@@ -95,15 +36,71 @@ def news_message(y_close, yy_close, difference):
 
         client = Client(account_sid, auth_token)
         message = client.messages.create(
-            body=f"TSLA: {direct}{difference}%\n Headline: {headline}.\n Brief: {description}",
+            body=f"TSLA: {direct}{round(price_def)}%\n Headline: {headline}.\n Brief: {description}",
             from_="whatsapp:+14155238886",
             to="whatsapp:+14147797306",
         )
-        print(message.body)
-        print(message.status)
+
+## STEP 1: Use https://newsapi.org/docs/endpoints/everything
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
+#HINT 2: Work out the value of 5% of yerstday's closing stock price.
 
 
-news_message(10, 25, 10)
+def formatter(my_year, my_month, my_day):
+    return f"{my_year}-{my_month:02d}-{my_day:02d}"
+
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day = now.day
+day_of_week = now.weekday()
+yesterday = ''
+day_yesterday = ''
+if day_of_week == 0:
+    yesterday = formatter(year, month, day - 3)
+    day_yesterday = formatter(year, month, day - 4)
+elif day_of_week == 1:
+    yesterday = formatter(year, month, day - 1)
+    day_yesterday = formatter(year, month, day - 4)
+elif day_of_week == 6:
+    yesterday = formatter(year, month, day - 2)
+    day_yesterday = formatter(year, month, day - 3)
+else:
+    yesterday = formatter(year, month, day - 1)
+    day_yesterday = formatter(year, month, day - 2)
+
+
+parameters = {
+"function": "TIME_SERIES_DAILY",
+"symbol": STOCK,
+"interval": "5min",
+"apikey": os.getenv("alpha_vantage")
+
+}
+
+response = requests.get(url="https://www.alphavantage.co/query", params=parameters)
+response.raise_for_status()
+stock_data = response.json()
+
+tesla_yesterday = stock_data['Time Series (Daily)'][yesterday]
+tesla_two_yesterday = stock_data['Time Series (Daily)'][day_yesterday]
+
+y_close = tesla_yesterday['4. close']
+
+close_percent = (5 * float(y_close) / 100)
+
+yy_close = tesla_two_yesterday['4. close']
+
+difference = float(y_close) - float(yy_close)
+if close_percent > difference:
+    news_message(y_close, yy_close, difference)
+## STEP 2: Use https://newsapi.org/docs/endpoints/everything
+# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
+#HINT 1: Think about using the Python Slice Operator
+
+
 ## STEP 3: Use twilio.com/docs/sms/quickstart/python
 # Send a separate message with each article's title and description to your phone number. 
 #HINT 1: Consider using a List Comprehension.
