b80f16d2004a146209a25bfa73197d5f500f0e9e
Rodgers31 <otienor986@gmail.com>
2025-04-28 18:42:36 -0500

    exercise project

--- exercise_google_sheets/main.py
+++ exercise_google_sheets/main.py
@@ -2,16 +2,28 @@ import requests
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+GENDER = 'male'
+WEIGHT_KG = '200'
+HEIGHT_CM = '175'
+AGE = '26'
 
-URL = 'https://trackapi.nutritionix.com'
+URL = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 load_dotenv('key.env')
 
+exercise = input("Tell me which exercise you did: ")
 
-parameters = {
+
+headers = {
     'Content-Type': 'application/json',
     'x-app-id': os.getenv("ID"),
     'x-app-key': os.getenv("KEY")
 }
 
-exercise = input("Tell me which exercise you did: ")
-response = requests.get(url=URL, headers=parameters, params=exercise)
+parameters = {
+    'query': exercise
+}
+
+
+response = requests.post(url=URL, json=parameters, headers=headers)
+result = response.json()
+print(result)
