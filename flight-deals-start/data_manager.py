c2454fed5f62ad259658ba0522534aa10375c2fb
Rodgers31 <otienor986@gmail.com>
2025-05-02 20:05:10 -0500

    flight deal getting sheety data

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -1,3 +1,20 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+URL = 'https://api.sheety.co/540b1e9afd000ccde8593508ff18f08c/flightDeals/prices'
+load_dotenv('key.env')
+
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.call_sheet()
+
+    def call_sheet(self):
+        bearer_headers = {
+            "Authorization": "Bearer " + os.getenv("AUTH")
+        }
+        response = requests.get(url=URL, headers=bearer_headers)
+        print(response)
