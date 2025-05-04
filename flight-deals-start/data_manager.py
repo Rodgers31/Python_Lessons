008fb9c3464fb2bb7414aeb7c42d29ce4196f572
Rodgers31 <otienor986@gmail.com>
2025-05-03 22:48:49 -0500

    flight deal getting sheety data and retuning

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+from pprint import pprint
 
 URL = 'https://api.sheety.co/540b1e9afd000ccde8593508ff18f08c/flightDeals/prices'
 load_dotenv('key.env')
@@ -17,4 +18,4 @@ class DataManager:
             "Authorization": "Bearer " + os.getenv("AUTH")
         }
         response = requests.get(url=URL, headers=bearer_headers)
-        print(response)
+        return response.json()
