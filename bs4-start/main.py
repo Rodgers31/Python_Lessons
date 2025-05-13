ce8707849aa8e67e2967279a67ed4248ea2f3392
Rodgers31 <otienor986@gmail.com>
2025-05-12 21:19:35 -0500

    Day 45 web-scrapping with beautiful soup

--- bs4-start/main.py
+++ bs4-start/main.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 
-with open(file="website.html") as f:
-    contents = f.read()
+with open("website.html", encoding="utf-8") as file:
+    contents = file.read()
     
 
 soup = BeautifulSoup(contents, "html.parser")
@@ -11,8 +11,15 @@ soup = BeautifulSoup(contents, "html.parser")
 # print(soup.title.string)
 
 all_anchor_tags = soup.find_all(name="p")
-print(all_anchor_tags)
+# print(all_anchor_tags)
 for tag in all_anchor_tags:
     tag.get("")
     # print(tag.getText())
 
+# isolate a specific h1 with id
+heading = soup.find(name="h1", id="name")
+# print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.getText())
+print(section_heading.name)
