b13948134e5cfe2b2b59c98413e2aa5532c568ad
Rodgers31 <otienor986@gmail.com>
2025-06-05 20:44:24 -0500

    Day 45 live website-scrapping with beautiful soup

--- bs4-start/websiteScrapping.py
+++ bs4-start/websiteScrapping.py
@@ -3,7 +3,13 @@ import requests
 
 
 
+response = requests.get("https://news.ycombinator.com/news")
+yc_webpage = response.text
 
-response = requests.get("https://appbrewery.github.io/news.ycombinator.com/")
-print(response.text)
+soup = BeautifulSoup(yc_webpage, "html.parser")
+print(soup.title)
+title = soup.find_all(class_="titleline")
+print(title)
+value = {i.text: i.find('a') for i in title}
+print(value)
 
