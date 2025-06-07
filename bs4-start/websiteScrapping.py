29400ba58e36179326ed8c2edbdd35b69729f755
Rodgers31 <otienor986@gmail.com>
2025-06-07 02:24:24 -0500

    Day 45 live website-scrapping with beautiful soup

--- bs4-start/websiteScrapping.py
+++ bs4-start/websiteScrapping.py
@@ -9,7 +9,13 @@ yc_webpage = response.text
 soup = BeautifulSoup(yc_webpage, "html.parser")
 print(soup.title)
 title = soup.find_all(class_="titleline")
-print(title)
-value = {i.text: i.find('a') for i in title}
-print(value)
+value = {i.text: i.find("a") for i in title}
+upvote = [int(score.getText().split()[0]) for score in soup.find_all(name='span', class_='score')]
+
+largest_number = max(upvote)
+largest_index = upvote.index(largest_number)
+print(largest_number)
+print(largest_index)
+# print(int(upvote[0].split()[0]))
+
 
