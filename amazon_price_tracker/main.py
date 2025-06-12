61291664d24558ba2950dcb5736bd46d55dd4fbf
Rodgers31 <otienor986@gmail.com>
2025-06-11 22:56:25 -0500

    Day 47 amazon price tracker

--- amazon_price_tracker/main.py
+++ amazon_price_tracker/main.py
@@ -1,12 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
-
-response = requests.get('https://appbrewery.github.io/instant_pot/')
+from smtplib import SMTP
+LINK = 'https://appbrewery.github.io/instant_pot/'
+response = requests.get(LINK)
 price = response.text
+MY_EMAIL = "rodgerdoger003@gmail.com"
+PASSWORD = "qyovfibexhjqoris"
 
 soup = BeautifulSoup(price, 'html.parser')
 
-item_price = soup.find("span", {"class": 'a-offscreen'}).getText()
+item_price = soup.find("span", {"class": 'a-offscreen'}).get_text()
+title = soup.find("span", {"id": "productTitle"}).get_text().strip()
 remove_dollar = item_price.split("$")[1]
 total_price = float(remove_dollar)
-print(total_price)
+title = title.replace(" ", '')
+
+if total_price < 100:
+    with SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs="rodgersotieno@myyahoo.com",
+                            msg=f"Subject:Amazon price alert!\n\n{title} is now ${total_price}\n\n"
+                                f"Buy now: {LINK}".encode("utf-8"))
+
