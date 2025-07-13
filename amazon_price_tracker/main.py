1e2a159a8620fba921ce2d31d6f9f489577bc45c
Rodgers31 <otienor986@gmail.com>
2025-07-13 12:11:48 -0500

    Day 47 amazon price tracker

--- amazon_price_tracker/main.py
+++ amazon_price_tracker/main.py
@@ -13,8 +13,9 @@ item_price = soup.find("span", {"class": 'a-offscreen'}).get_text()
 title = soup.find("span", {"id": "productTitle"}).get_text().strip()
 remove_dollar = item_price.split("$")[1]
 total_price = float(remove_dollar)
-title = title.replace(" ", '')
 
+title = title.replace(" ", '')
+print(total_price)
 if total_price < 100:
     with SMTP("smtp.gmail.com") as connection:
         connection.starttls()
@@ -22,4 +23,4 @@ if total_price < 100:
         connection.sendmail(from_addr=MY_EMAIL, to_addrs="rodgersotieno@myyahoo.com",
                             msg=f"Subject:Amazon price alert!\n\n{title} is now ${total_price}\n\n"
                                 f"Buy now: {LINK}".encode("utf-8"))
-
+        print(connection.getreply())
