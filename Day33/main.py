3165db3ab0d6399171e883fd5091d195d421c341
roger <otienor986@gmail.com>
2025-10-05 00:12:05 -0500

    Day 47 amazon price tracker

--- Day33/main.py
+++ Day33/main.py
@@ -17,6 +17,7 @@ parameters = {
     "formatted": 0
 }
 
+
 response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
