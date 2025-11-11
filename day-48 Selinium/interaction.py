f6afcbf13c69b3625404ba603d509d4ee7348de6
roger <otienor986@gmail.com>
2025-11-10 22:08:21 -0600

    Day 48 Cookies clicker game solution

--- day-48 Selinium/interaction.py
+++ day-48 Selinium/interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
@@ -9,5 +10,13 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://en.wikipedia.org/wiki/Main_Page')
 wiki = driver.find_element(By.CSS_SELECTOR, '#articlecount a')
 wiki.click()
-
+driver.get('https://en.wikipedia.org/wiki/Main_Page')
+# Find elements by link text
 wiki_portals = driver.find_element(By.LINK_TEXT, 'Content portals')
+wiki_portals.click()
+
+#Find the "Search" <input> name
+search = driver.find_element(By.NAME, 'search')
+# Sending Keyboard input to selenium
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
